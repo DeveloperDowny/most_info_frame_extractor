c67d61ff05bbd0aee692be50d82ec28f961fc1d0
Vedant Panchal <60831483+DeveloperDowny@users.noreply.github.com>
2024-12-24 17:45:38 +0530

    feat: commenting out unnecessary features and commenting out data plotter related code

--- v2/input_strategy_factory.py
+++ v2/input_strategy_factory.py
@@ -1,8 +1,9 @@
 from youtube_video_url_input_strategy import YouTubeVideoURLInputStrategy
 from input_strategy import InputStrategy
-from local_video_input_strategy import LocalVideoInputStrategy
-from python_object_input_strategy import PythonObjectInputStrategy
-from playlist_input_strategy import PlaylistInputStrategy
+
+# from local_video_input_strategy import LocalVideoInputStrategy
+# from python_object_input_strategy import PythonObjectInputStrategy
+# from playlist_input_strategy import PlaylistInputStrategy
 from input_data.input_data import InputData
 
 
@@ -15,7 +16,7 @@ class InputStrategyFactory:
         extraction_strategy,
         ocr_approval_strategy,
         input_data: InputData = None,
-        storage_strategy = None,
+        storage_strategy=None,
     ) -> InputStrategy:
         if input_type == "youtube":
             if input_data == None:
@@ -28,29 +29,29 @@ class InputStrategyFactory:
                 ocr_strategy,
                 extraction_strategy,
                 ocr_approval_strategy,
-                storage_strategy
+                storage_strategy,
             )
-        elif input_type == "local":
-            directory = input("Enter directory path: ")
-            return LocalVideoInputStrategy(
-                directory, ocr_strategy, extraction_strategy, ocr_approval_strategy
-            )
-        elif input_type == "object":
-            """The directory path should be like this `xxxxxx_python_object`"""
-            directory = input("Enter directory path: ")
-            return PythonObjectInputStrategy(
-                directory, ocr_strategy, extraction_strategy
-            )
-        elif input_type == "playlist":
-            playlist_url = input("Enter YouTube playlist URL: ")
-            start_from = int(input("Enter start from: "))
+        # elif input_type == "local":
+        #     directory = input("Enter directory path: ")
+        #     return LocalVideoInputStrategy(
+        #         directory, ocr_strategy, extraction_strategy, ocr_approval_strategy
+        #     )
+        # elif input_type == "object":
+        #     """The directory path should be like this `xxxxxx_python_object`"""
+        #     directory = input("Enter directory path: ")
+        #     return PythonObjectInputStrategy(
+        #         directory, ocr_strategy, extraction_strategy
+        #     )
+        # elif input_type == "playlist":
+        #     playlist_url = input("Enter YouTube playlist URL: ")
+        #     start_from = int(input("Enter start from: "))
 
-            return PlaylistInputStrategy(
-                playlist_url,
-                start_from,
-                ocr_strategy,
-                extraction_strategy,
-                ocr_approval_strategy,
-            )
+        #     return PlaylistInputStrategy(
+        #         playlist_url,
+        #         start_from,
+        #         ocr_strategy,
+        #         extraction_strategy,
+        #         ocr_approval_strategy,
+        #     )
         else:
             raise ValueError("Invalid input type")

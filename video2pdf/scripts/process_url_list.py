976c25c2c50754b91461fa3019e93b044b0c0e32
Vedant Panchal <60831483+DeveloperDowny@users.noreply.github.com>
2025-05-28 20:49:43 +0530

    feat: updated process url list

--- video2pdf/scripts/process_url_list.py
+++ video2pdf/scripts/process_url_list.py
@@ -1,6 +1,9 @@
 import subprocess
 import time
 
+# from video2pdf.utils.helper import Helper
+from video2pdf.utils.helper import Helper
+
 
 # from video2pdf.utils.helper import Helper
 
@@ -8,22 +11,17 @@ import time
 def main(urls):
     for url in urls:
         try:
-            subprocess.run(
-                [
-                    "python", "/home/vedant/Desktop/glimpsify/most_info_frame_extractor/video2pdf/main.py",
-                    # "--input=youtube",
-                    "--input=pickle",
-                    # f"--dir={url}", "--ocr=tesseract", "--extraction=k_transactions", "--extraction=prominent_peak"
-                    f"--dir=whsuyw_zcp_python_object", "--ocr=tesseract", "--extraction=prominent_peaks"
-                ]
-            )
+            subprocess.run(["python", "/home/vedant/Desktop/glimpsify/most_info_frame_extractor/video2pdf/main.py",
+                            "--input=youtube", f"--url={url}", "--cleanup"])
             time.sleep(10)
         except:
             print("Exception occurred for: ", url)
 
 
 if __name__ == "__main__":
-    playlist_url = ""
-    # urls = Helper.get_video_urls_from_playlist(playlist_url)
-    urls = ["https://www.youtube.com/watch?v=j2-9yymHfeU&list=TLGGEGsAE2D2xJgxNTA1MjAyNQ"]
+    playlist_url = "https://youtube.com/playlist?list=PLHENddstfsnpEHBWgt9yddEVDfcpQdUg7&si=t0DCVsJaguKzBoPP"
+    # playlist_url = "https://youtube.com/playlist?list=PL4gu8xQu0_5KiYnRlueicckEmpFAiRD5Y&si=XeTFcmjXdbE4irJW"
+    urls = Helper.get_video_urls_from_playlist(playlist_url)
+    # urls = ["https://www.youtube.com/watch?v=j2-9yymHfeU&list=TLGGEGsAE2D2xJgxNTA1MjAyNQ"]
+    # urls = urls[:1]
     main(urls)

135075fffb575a940e1ee4bd53fa6143f96833b4
Vedant Panchal <60831483+DeveloperDowny@users.noreply.github.com>
2025-06-01 22:40:37 +0530

    fix: BASE_DIR init when env variable not specified issue

--- video2pdf/utils/constants.py
+++ video2pdf/utils/constants.py
@@ -2,9 +2,12 @@ import os
 from pathlib import Path
 
 module_dir = os.path.dirname(__file__)
-BASE_DIR = Path(os.getenv("BASE_DIR", None))
+# TODO: Use pydantic_settings in the future
+BASE_DIR = os.getenv("BASE_DIR", None)
 if not BASE_DIR:
     BASE_DIR = Path(module_dir).joinpath("../data").resolve()
+else:
+    BASE_DIR = Path(BASE_DIR)
 BASE_DIR.mkdir(exist_ok=True, parents=True)
 # BASE_DIR = Path(module_dir).joinpath("../archives/data_archive_50_python_objects_all_dirs_phash_approval_strategy_v3").resolve()
 BASE_DIR = str(BASE_DIR)
